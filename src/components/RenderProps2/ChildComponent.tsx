6006135980f695ede1a51ee085d80a9844ef646f
Juan Gómez <gb.jc@outlook.com>
2025-01-16 12:49:43 -0500

    Render props and composition.

--- src/components/RenderProps2/ChildComponent.tsx
+++ src/components/RenderProps2/ChildComponent.tsx
@@ -1,7 +1,16 @@
-const ChildComponent = () => {
+import { FC } from "react";
+
+interface ChildComponentProps {
+	data: string[]
+}
+
+const ChildComponent: FC<ChildComponentProps> = ({ data }) => {
   return (
-    <>
-    </>
+		data.map((value, index) => (
+			<li key={index}>
+				{ value }
+			</li>
+		))
   );
 };
 
--- src/components/RenderProps2/ParentComponent.tsx
+++ src/components/RenderProps2/ParentComponent.tsx
@@ -1,4 +1,17 @@
-const ParentComponent = () => {
+import { FC, ReactNode, useState } from "react";
+
+interface ParentCompoentProps {
+	render: (data: string[]) => ReactNode
+}
+
+const ParentComponent: FC<ParentCompoentProps> = ({ render }) => {
+	const [data] = useState<string[]>(["Apple 🍎", "Banana 🍌", "Orange 🍊"])
+
+	return (
+		<ul>
+			{ render(data) }
+		</ul>
+	)
 };
 
 export default ParentComponent;
