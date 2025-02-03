36e0ad12d9a927b93cde658db13b7e24752f0841
Juan Gómez <gb.jc@outlook.com>
2025-02-03 12:49:03 -0500

    Controlled and uncontrolled components

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 export const ControlledInput = () => {
+	const [value, setValue] = useState('')
+	
   return (
-    <div>
+    <div>			
+			<input
+				type="text"
+				value={ value }
+				placeholder="Cupon code"
+				onChange={ (e) => setValue(e.target.value) }
+			/>
+			<p>
+				Cupon code: <b>{value}</b>
+			</p>
     </div>
   );
 };
