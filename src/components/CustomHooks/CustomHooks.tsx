262cb5fbb2c108154eddfbbaf00831649fa7407d
Juan Gómez <gb.jc@outlook.com>
2025-02-03 11:41:16 -0500

    Custom hooks

--- src/components/CustomHooks/CustomHooks.tsx
+++ src/components/CustomHooks/CustomHooks.tsx
@@ -1,10 +1,38 @@
-const UserComponent = () => {
+import { FC, useState } from "react";
+
+const useLoading = (initialLoading: boolean = false) => {
+	const [isLoading, setIsLoading] = useState<boolean>(initialLoading)
+
+	return {
+		isLoading,
+		setIsLoading
+	}
+}
+
+type UserProps = {
+	name: string
+}
+
+const UserComponent: FC<UserProps> = ({ name }) => {
+	const { isLoading, setIsLoading } = useLoading(false)
+
+	if(isLoading) {
+		return (
+			<div>
+				<p>Loading...</p>
+				<button onClick={ () => setIsLoading(!isLoading) }>Change loading state</button>
+			</div>
+		)
+	}
+
   return (
     <div>
+			<p>Hello, {name}</p>
+			<button onClick={ () => setIsLoading(!isLoading) }>Change loading state</button>
     </div>
   );
 };
 
 export const ParentComponent = () => {
-  return <UserComponent />;
+  return <UserComponent name={ 'Juan' }/>;
 };
