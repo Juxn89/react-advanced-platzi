76249b18452190dba0bf23456a948b73a3d46ad4
Juan Gómez <gb.jc@outlook.com>
2025-02-03 11:23:28 -0500

    High Order Component

--- src/components/HighOrderComponent/HighOrderComponent.tsx
+++ src/components/HighOrderComponent/HighOrderComponent.tsx
@@ -1,6 +1,27 @@
+import React, { FC } from "react";
+
+function withLoading<T> (Component: React.ComponentType<T>) {
+	return (props: T & { isLoading: boolean }) => {
+		const { isLoading, ...rest } = props
+		return isLoading ? <p>Loading...</p> : <Component {...rest as T}/>
+	}
+}
+
+type UserProps = {
+	name: string
+}
+
+const UserComponent: FC<UserProps> = ({name}) => {
+	return <p>Hellor, {name}!</p>
+}
+
+const UserWithLoading = withLoading(UserComponent)
+
 export const ParentComponent = () => {
   return (
     <div>
+			<UserWithLoading isLoading={ true } name="Juan"/>
+			<UserWithLoading isLoading={ false} name="David Martín"/>
     </div>
   );
 };
