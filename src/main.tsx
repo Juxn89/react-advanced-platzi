78b3d6f04fa48c4727946d91c66e447aff178251
Juan Gómez <gb.jc@outlook.com>
2025-02-06 12:29:57 -0500

    Clean up the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TodoList } from './components/TodoList'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TodoList />
+		
   </StrictMode>,
 )
