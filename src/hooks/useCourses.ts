54014d7f47113f677c2f6c7226a1bac9f4a3c8dc
Juan Gómez <gb.jc@outlook.com>
2025-02-07 11:49:39 -0500

    Clean up the project, added TankStack Query package and custom hook for requestion to API.

---
+++ src/hooks/useCourses.ts
@@ -0,0 +1,24 @@
+import { useQuery, QueryFunction } from "@tanstack/react-query"
+
+interface Course {
+	id: number,
+	title: string,
+	description: string,
+	duration: string
+}
+
+const fetchCourses: QueryFunction<Course[]> = async () => {
+	const response = await fetch('/api/courses.json')
+
+	if(!response.ok)
+		throw new Error('Network response was not OK')
+
+	return response.json()
+}
+
+export const useCourses = () => {
+	return useQuery<Course[], Error>({
+		queryKey: ['courses'],
+		queryFn: fetchCourses
+	})
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { StateMachineWizard } from './App'
+import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-		<StateMachineWizard />
+		<App />
   </StrictMode>,
 )
