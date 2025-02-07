406537172728a5da7ee16c8aa0290b7247913b0d
Juan Gómez <gb.jc@outlook.com>
2025-02-07 12:36:05 -0500

    Paggination with Concurrent Mode and useTransition

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { lazy, Suspense } from "react"
+import { lazy, Suspense, useState, useTransition } from "react"
 import { useCourses } from "./hooks/useCourses"
 
 const CourseList = lazy( () => import('./components/CourseList') )
 
 export const App = () => {
 	const {data: courses, isLoading, error } = useCourses()
+	const [currentPage, setCurrentPage] = useState(1)
+
+	const coursesPerPage = 2
+	const [isPending, startTransition] = useTransition()
 
 	if(isLoading)
 		return <div>Loading...</div>
@@ -20,6 +24,19 @@ export const App = () => {
 			<h1>✨ TanStack Query ✨</h1>
 			<Suspense fallback={ <p>Loading...</p> }>
 				<CourseList courses={ courses } />
+				<div>
+					{
+						Array.from({ length: Math.ceil(courses.length / coursesPerPage) }, (_, index) => (
+							<button
+								key={ index }
+								onClick={ () => { setCurrentPage(index + 1) } }
+							>
+								{ index + 1 }
+							</button>
+						))
+					}
+				</div>
+				{ isPending && <div>Loading new page...</div> }
 			</Suspense>
 		</section>
 	)
